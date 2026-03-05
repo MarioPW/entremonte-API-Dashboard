dc8406d6644385c4b4457f6f0a4f2acdc251f647
marioPW <mariotriana1978@gmail.com>
2026-03-05 03:03:42 -0500

    Feat: build.sh updated5

--- build.sh
+++ build.sh
@@ -16,9 +16,17 @@ python manage.py migrate
 # 4. Creación de superusuario (Opcional/Segura)
 # El flag || true evita que el script falle si el usuario ya existe.
 # Ya NO borramos al usuario antes de este paso.
-if [ "$DJANGO_SUPERUSER_EMAIL" ]; then
-    python manage.py createsuperuser \
-        --noinput \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --first_name "Admin" || true
-fi
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+email = '$DJANGO_SUPERUSER_EMAIL';
+password = '$DJANGO_SUPERUSER_PASSWORD';
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password, first_name='Admin')
+    print('Usuario creado con éxito')
+else:
+    user = User.objects.get(email=email)
+    user.set_password(password)
+    user.save()
+    print('Password de usuario actualizado')
+"
