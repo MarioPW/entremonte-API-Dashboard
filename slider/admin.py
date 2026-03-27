a2ccca3c1c5fb8de8c892f00c6613319b7151374
marioPW <mariotriana1978@gmail.com>
2026-03-27 18:57:55 -0500

    Feat: add link (id) to slider list

--- slider/admin.py
+++ slider/admin.py
@@ -3,6 +3,10 @@ from .models import Slider
 
 @admin.register(Slider)
 class Slider_Admin(admin.ModelAdmin):
-    list_display = ('title', 'url', 'message')
-    list_display_links = ('title',)
+    def short_id(self, obj):
+        return str(obj.id)[:5] + '...'
+    short_id.short_description = 'ID (abreviado)'
+    
+    list_display = ('short_id', 'title', 'url', 'message')
+    list_display_links = ('short_id', 'title','message',)
     search_fields = ('title',)
